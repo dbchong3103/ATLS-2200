2b986e6ee82c0261e0fca6930784716ec64bd3b1
dbchong3103 <dach8404@colorado.edu>
2023-05-09 17:28:37 -0600

    final

---
+++ final/final.js
@@ -0,0 +1,33 @@
+// window.onLoad = resize();
+// window.addEventListener('resize', function(e){resize();});
+// function resize() {
+//   this.w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//   this.h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+//   document.getElementById('svg').setAttribute('width', this.w);
+//   document.getElementById('svg').setAttribute('height', this.h*0.75);
+// }
+
+// Get the modal
+var modal = document.getElementById('myModal');
+ 
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('img');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.alt;
+}
+ 
+ 
+// When the user clicks on <span> (x), close the modal
+modal.onclick = function() {
+    img01.className += " out";
+    setTimeout(function() {
+       modal.style.display = "none";
+       img01.className = "modal-content";
+     }, 400);
+    
+ }   
